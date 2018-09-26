d406967a1b3c375b412b265c1488f9603479f955
David Tolpin <david.tolpin@gmail.com>
2018-09-26 16:26:31 +0300

    more tests

--- ad/ad_test.go
+++ ad/ad_test.go
@@ -10,6 +10,8 @@ import (
 	"testing"
 )
 
+// Tooling for comparing models
+
 // wrapper around model body
 func srcModel(body string) string {
 	return fmt.Sprintf(`package mymodel
@@ -27,7 +29,6 @@ func (m *interface{}) Observe (parameters []float64) (float64, []float64) {
 %s
 }`, body)
 }
-
 
 func sourcesEqual(got, expected string) bool {
 	// normalize source code by parsing and printing
@@ -55,23 +56,28 @@ func sourcesEqual(got, expected string) bool {
 	return gotBuffer.String() == expectedBuffer.String()
 }
 
+// check that templates work and parsing normalizes
 func TestSourcesEqual(t *testing.T) {
-	for _, c := range []struct {
-		got, expected string
-		equal bool
-	}{
-		{"return 0.", "return 0.", true},
-		{"return 0.", "return    0.", true},
-		{"return 0.", "return 1.", false},
-		{"return 0.", "return 0", false},
-	} {
-		if sourcesEqual(srcModel(c.got), srcModel(c.expected)) != c.equal {
-			t.Errorf("'%s' and '%s' should %sbe equal",
-				c.got, c.expected,
-				map[bool]string {
-					false: "not ",
-					true: "",
-				}[c.equal])
+	for _, m := range []func (string) string {srcModel, diffModel} {
+		for _, c := range []struct {
+			got, expected string
+			equal bool
+		}{
+			{"return 0.", "return 0.", true},
+			{"return 0.", "return    0.", true},
+			{"return 0.", "return 1.", false},
+			{"return 0.", "return 0", false},
+		} {
+			g := m(c.got)
+			e := m(c.expected)
+			if sourcesEqual(g, e) != c.equal {
+				t.Errorf("'%s' and '%s' should %sbe equal",
+					g, e,
+					map[bool]string {
+						false: "not ",
+						true: "",
+					}[c.equal])
+			}
 		}
 	}
 }
