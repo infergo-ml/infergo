ad11b1a4f81aac3caf486a8b6c1541199b332bd4
David Tolpin <david.tolpin@gmail.com>
2018-10-12 23:55:26 +0300

    nice layout for tape

--- ad/tape.go
+++ ad/tape.go
@@ -2,18 +2,36 @@ package ad
 
 // Implementation of the AD tape
 
+// A Tape consists of the list of records, as well as memory
+// for pointers and stored intermediate values.
 type Tape struct {
-	records []record             // built during forward pass
-	bars    map[*float64]float64 // filled during backward pass
-	stack   []float64            // used for passing arguments
-	rc, sc  int                  // record and stack counters
+	records    []record   // recorded instructions
+	lvalues    []*float64 // addresses of locations
+	rvalues    []float64  // stored intermediate values
+	elementals []uintptr  // elemental function indices
 }
 
-const maxarg int = 2
-
+// A record specifies the record type and indexes the tape memory
+// to specify record argument. At the cost of one redirection,
+// the number of memory allocations is logarithmic in the number
+// of instructions.
 type record struct {
-	typ, opcode int // record type and opcode
-	value       float64
-	parameters  [maxarg]float64
-	args        [maxarg]*float64
+	typ, op int // record type and opcode or index of elemental
+	il, ir  int // indices of the first pointer and value
 }
+
+// Record types
+const (
+	typAssignment = iota
+	typArithmetic
+	typElemental
+)
+
+// Arithmetic operations
+const (
+	opNeg = iota
+	opAdd
+	opSub
+	opMul
+	opDiv
+)
