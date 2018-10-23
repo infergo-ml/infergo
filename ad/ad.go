0c4ee2b592b0a4626c59fc71fc1939ccaf51b751
David Tolpin <david.tolpin@gmail.com>
2018-10-23 09:46:05 +0300

    more documentation

--- ad/ad.go
+++ ad/ad.go
@@ -2,9 +2,7 @@
 package ad
 
 // A model is defined in it's own package. The model must
-// implement interface model.Model. Interface model.DiffModel is
-// induced through automatic differentiation.
-// In the model's source code:
+// implement interface model.Model. In the model's source code:
 //   1. Method Observe of interface model.Model is
 //      differentiated.
 //   2. All methods on the type implementing model.Model
@@ -15,16 +13,26 @@ package ad
 //      c) standalone calls to methods on the type implementing
 //         model.Model (apparently called for side  effects on
 //         the model).
+//
 // Functions are considered elementals (and must have a
 // registered derivative):
 //   a) if they are defined in a different package, and
 //   b) their signature is of kind 
 //                  func (float64, ...float64) float64
 //      that is, at least one float64 argument and float64 return
-//      value.
+//      value. For example, function
+//                  func (float64, float64, float64) float64
+//      is considered elemental, while functions
+//                  func ([]float64) float64
+//                  func (int, float64) float64
+//      are not.
+//  
 // Derivatives do not propagate through a function that is not
 // an elemental or a call to a model method. If a derivative is
 // not registered for an elemental, calling the elemental in a
 // differentiated context will cause a run-time error.
+//
+// The differentiated model is put into subpackage 'ad'
+// of the model's package.
 
 import ()
