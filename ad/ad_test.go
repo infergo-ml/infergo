9760305b36609c2996ca87fc17a2da0761383fdc
David Tolpin <david.tolpin@gmail.com>
2018-10-25 21:55:49 +0300

    unit tests for collectFiles

--- ad/ad_test.go
+++ ad/ad_test.go
@@ -2,7 +2,6 @@ package ad
 
 import (
 	_ "bytes"
-	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
@@ -13,12 +12,11 @@ import (
 
 // The input to ad routines is a parsed package. Let's
 // emulate parsing a package on scripts.
-func parseTestModel(m *model, sources []string) {
+func parseTestModel(m *model, sources map[string]string) {
 	m.fset = token.NewFileSet()
 
 	// parse it
-	for i, source := range sources {
-		fname := fmt.Sprintf("file_%v", i)
+	for fname, source := range sources {
 		if file, err := parser.ParseFile(
 			m.fset, fname, source, 0); err == nil {
 			name := file.Name.Name
@@ -37,12 +35,12 @@ func parseTestModel(m *model, sources []string) {
 
 func TestCollectTypes(t *testing.T) {
 	for _, c := range []struct {
-		model []string
+		model map[string]string
 		types map[string]bool
 	}{
 		// Single model
-		{[]string{
-			`package single
+		{map[string]string{
+            "single.go": `package single
 
 type Model float64
 
@@ -56,8 +54,8 @@ func (m Model) Observe(x []float64) float64 {
 			}},
 
 		// No model
-		{[]string{
-			`package nomodel
+		{map[string]string{
+            "nomodel.go": `package nomodel
 
 type Model float64
 
@@ -69,8 +67,8 @@ func (m Model) observe(x []float64) float64 {
 			map[string]bool{}},
 
 		// Two models
-		{[]string{
-			`package double
+		{map[string]string{
+            "double.go": `package double
 
 type ModelA float64
 type ModelB float64
@@ -96,12 +94,12 @@ func (m ModelB) Observe(x []float64) float64 {
 			t.Errorf("failed to check model %v: %s",
 				m.pkg.Name, err)
 		}
-		modelTypes, err := m.collectTypes()
-		if len(modelTypes) > 0 && err != nil {
+		mtypes, err := m.collectTypes()
+		if len(mtypes) > 0 && err != nil {
 			// Ignore the error when there is no model
 			panic(err)
 		}
-		for _, mt := range modelTypes {
+		for _, mt := range mtypes {
 			if !c.types[mt.String()] {
 				t.Errorf("model %v: type %v is not a model",
 					m.pkg.Name, mt)
@@ -116,6 +114,97 @@ func (m ModelB) Observe(x []float64) float64 {
 }
 
 func TestCollectFiles(t *testing.T) {
+	for _, c := range []struct {
+		model map[string]string
+		fnames map[string]bool
+	}{
+		// Single file, two methods
+		{map[string]string{
+            "single.go": `package single
+
+type Model float64
+
+func (m Model) Observe(x []float64) float64 {
+	return - float64(m) * x[0]
+}
+
+func (m Model) Sample() float64 {
+	return 0.0
+}
+`,
+		},
+			map[string]bool{
+				"single.go": true,
+			}},
+
+		// Two files, first file contains the methods
+		{map[string]string{
+            "first.go": `package first
+
+type Model float64
+
+func (m Model) Observe(x []float64) float64 {
+	return - float64(m) * x[0]
+}
+`,
+            "second.go": `package first
+
+func foo() {
+}
+`,
+		},
+			map[string]bool{
+                "first.go": true,
+            }},
+
+		// Two files, first file contains the methods
+		{map[string]string{
+            "first.go": `package both
+
+type Model float64
+
+func (m *Model) Observe(x []float64) float64 {
+	return - float64(*m) * x[0]
+}
+`,
+            "second.go": `package both
+
+func (m Model) Sample() float64 {
+	return 0.0
+}
+`,
+		},
+			map[string]bool{
+                "first.go": true,
+                "second.go": true,
+            }},
+	} {
+		m := &model{}
+		parseTestModel(m, c.model)
+		err := m.check(m.pkg.Name)
+		if err != nil {
+			t.Errorf("failed to check model %v: %s",
+				m.pkg.Name, err)
+		}
+        mtypes, err := m.collectTypes()
+		mfiles, err := m.collectFiles(mtypes)
+		if len(mfiles) > 0 && err != nil {
+			// Ignore the error when there is no model
+			panic(err)
+		}
+		for _, mf := range mfiles {
+            fname := m.fset.Position(mf.Package).Filename
+			if !c.fnames[fname] {
+				t.Errorf("model %v: file %q contains no methods",
+					m.pkg.Name, fname)
+			}
+			delete(c.fnames, fname)
+		}
+		for k := range c.fnames {
+			t.Errorf("model %v: file %q was not collected",
+				m.pkg.Name, k)
+		}
+	}
 }
 
 func TestCollectMethods(t *testing.T) {
