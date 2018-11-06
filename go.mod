6ba8bed0038639307cba2b5fb499d454df7fd5f8
David Tolpin <david.tolpin@gmail.com>
2018-11-06 21:06:15 +0200

    hmc in progress

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module bitbucket.org/dtolpin/infergo
 
-require golang.org/x/tools v0.0.0-20181102223251-96e9e165b75e
+require golang.org/x/tools v0.0.0-20181105230042-78dc5bac0cac
--- infer/mcmc.go
+++ infer/mcmc.go
@@ -1,3 +1,92 @@
 package infer
 
 // Markov Chain Monte Carlo for approximation of the posterior.
+
+import (
+	"bitbucket.org/dtolpin/infergo/ad"
+	"bitbucket.org/dtolpin/infergo/model"
+	"math"
+	"math/rand"
+)
+
+// MCMC is the interface of MCMC samplers.
+type MCMC interface {
+	Sample(
+		m model.Model,
+		x []float64,
+		samples chan<- []float64,
+	)
+}
+
+// energy computes the energy of a particle; used
+// by HMC variants.
+func energy(l float64, r []float64) float64 {
+	k := 0.
+	for i := 0; i != len(r); i++ {
+		k += r[i] * r[i]
+	}
+	return l + 0.5*k
+}
+
+// leapfrog advances x and r a single 'leapfrog'; used
+// by HMC variants.
+func leapfrog(
+	m model.Model,
+	gradp *[]float64,
+	x []float64,
+	r []float64,
+	eps float64,
+) (l float64) {
+	for i := 0; i != len(x); i++ {
+		r[i] += 0.5 * eps * (*gradp)[i]
+		x[i] += eps * r[i]
+	}
+	l, *gradp = m.Observe(x), ad.Gradient()
+	for i := 0; i != len(x); i++ {
+		r[i] += 0.5 * eps * (*gradp)[i]
+	}
+	return l
+}
+
+// Vanilla Hamiltonian Monte Carlo Sampler.
+type HMC struct {
+	L   int     // number of leapfrog steps
+	Eps float64 // leapfrog step size
+}
+
+func (hmc *HMC) Sample(
+	m model.Model,
+	x []float64,
+	samples chan<- []float64,
+) {
+	backup := make([]float64, len(x))
+	for {
+		// sample the next r
+		r := make([]float64, len(x))
+		for i := 0; i != len(x); i++ {
+			r[i] = rand.NormFloat64()
+		}
+
+		// Back up the current value of x
+		copy(backup, x)
+
+		l0, grad := m.Observe(x), ad.Gradient()
+		e0 := energy(l0, r) // initial energy
+		var l float64
+		for i := 0; i != hmc.L; i++ {
+			l = leapfrog(m, &grad, x, r, hmc.Eps)
+		}
+		e := energy(l, r) // final energy
+
+		// Accept with MH probability.
+		if e-e0 < math.Log(1.-rand.Float64()) {
+			// Rejected, restore x from backup.
+			copy(x, backup)
+		}
+
+		// write a sample to the channel
+		sample := make([]float64, len(x))
+		copy(sample, x)
+		samples <- sample
+	}
+}
