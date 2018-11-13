3a347bfc3e344cfa3dcb36c5b81f11f16aba764b
David Tolpin <david.tolpin@gmail.com>
2018-11-13 14:11:37 +0200

    reformatted

--- dist/dist_test.go
+++ dist/dist_test.go
@@ -19,22 +19,22 @@ func TestNormal(t *testing.T) {
 	} {
 		logv := 2 * math.Log(c.stddev)
 		ll := Normal.Logps(c.mean, logv, c.y...)
-		if math.Abs(ll - c.ll) > 1E-6 {
+		if math.Abs(ll-c.ll) > 1E-6 {
 			t.Errorf("Wrong logpdf of Normal(%.v|%.v, %.v): "+
 				"got %.4g, want %.4g",
 				c.y, c.mean, c.stddev, ll, c.ll)
 		}
 		llo := Normal.Observe(append([]float64{c.mean, logv}, c.y...))
-		if math.Abs(ll - llo) > 1E-6 {
-			t.Errorf("Wrong result of Observe([%.4g, %.4g, %v...]): " +
+		if math.Abs(ll-llo) > 1E-6 {
+			t.Errorf("Wrong result of Observe([%.4g, %.4g, %v...]): "+
 				"got %.4g, want %.4g",
 				c.mean, logv, c.y, llo, ll)
 		}
 		if len(c.y) == 1 {
 			ll1 := Normal.Logp(c.mean, logv, c.y[0])
-			if math.Abs(ll - ll1) > 1E-6 {
-				t.Errorf("Wrong result of Logp(%.4g, %.4g, %.4g): " +
-					"got %.4g, want %.4g", 
+			if math.Abs(ll-ll1) > 1E-6 {
+				t.Errorf("Wrong result of Logp(%.4g, %.4g, %.4g): "+
+					"got %.4g, want %.4g",
 					c.mean, logv, c.y[0], ll1, ll)
 			}
 		}
@@ -53,21 +53,21 @@ func TestExpon(t *testing.T) {
 	} {
 		logl := math.Log(c.lambda)
 		ll := Expon.Logps(logl, c.y...)
-		if math.Abs(ll - c.ll) > 1E-6 {
+		if math.Abs(ll-c.ll) > 1E-6 {
 			t.Errorf("Wrong logpdf of Expon(%.v|%.v): "+
 				"got %.4g, want %.4g",
 				c.y, c.lambda, ll, c.ll)
 		}
 		llo := Expon.Observe(append([]float64{logl}, c.y...))
-		if math.Abs(ll - llo) > 1E-6 {
-			t.Errorf("Wrong result of Observe([%.4g, %v...]): " +
+		if math.Abs(ll-llo) > 1E-6 {
+			t.Errorf("Wrong result of Observe([%.4g, %v...]): "+
 				"got %.4g, want %.4g",
 				logl, c.y, llo, ll)
 		}
 		if len(c.y) == 1 {
 			ll1 := Expon.Logp(logl, c.y[0])
-			if math.Abs(ll - ll1) > 1E-6 {
-				t.Errorf("Wrong result of Logp(%.4g, %.4g): " +
+			if math.Abs(ll-ll1) > 1E-6 {
+				t.Errorf("Wrong result of Logp(%.4g, %.4g): "+
 					"got %.4g, want %.4g",
 					logl, c.y[0], ll1, ll)
 			}
