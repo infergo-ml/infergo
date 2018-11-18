4f047eec1e57b76753ad5ee514f5d874ee443d6f
David Tolpin <david.tolpin@gmail.com>
2018-11-18 03:02:30 +0200

    reformatted

--- examples/hello/model/ad/model.go
+++ examples/hello/model/ad/model.go
@@ -1,8 +1,8 @@
 package model
 
 import (
-	"bitbucket.org/dtolpin/infergo/ad"
 	. "bitbucket.org/dtolpin/infergo/dist/ad"
+	"bitbucket.org/dtolpin/infergo/ad"
 	"math"
 )
 
@@ -16,7 +16,7 @@ func (m *Model) Observe(x []float64) float64 {
 	} else {
 		ad.Setup(x)
 	}
-	return ad.Return(ad.Call(func(_vararg []float64) {
+	return ad.Return(ad.Call(func(_ []float64) {
 		Normal.Logps(0, 0, m.Data...)
 	}, 2, &x[0], ad.Elemental(math.Exp, &x[1])))
 }
--- examples/schools/model/ad/model.go
+++ examples/schools/model/ad/model.go
@@ -1,16 +1,16 @@
 package model
 
 import (
-	"bitbucket.org/dtolpin/infergo/ad"
 	. "bitbucket.org/dtolpin/infergo/dist/ad"
+	"bitbucket.org/dtolpin/infergo/ad"
 	"math"
 )
 
 type Model struct {
-	J          int
-	Y          []float64
-	Sigma      []float64
-	Stau, Seta float64
+	J		int
+	Y		[]float64
+	Sigma		[]float64
+	Stau, Seta	float64
 }
 
 func (m *Model) Observe(x []float64) float64 {
@@ -30,7 +30,7 @@ func (m *Model) Observe(x []float64) float64 {
 	ad.Assignment(&ll, ad.Call(func(_ []float64) {
 		Normal.Logp(0, 0, 0)
 	}, 3, ad.Value(0), &m.Stau, &x[1]))
-	ad.Assignment(&ll, ad.Arithmetic(ad.OpAdd, &ll, ad.Call(func(_vararg []float64) {
+	ad.Assignment(&ll, ad.Arithmetic(ad.OpAdd, &ll, ad.Call(func(_ []float64) {
 		Normal.Logps(0, 0, eta...)
 	}, 2, ad.Value(0), &m.Seta)))
 	for i, y := range m.Y {
