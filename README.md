736f58372364c8dbd7e5e03e01060ab0de2407ae
David Tolpin <david.tolpin@gmail.com>
2018-11-21 10:57:11 +0200

    prior in example

--- README.md
+++ README.md
@@ -22,7 +22,11 @@ type Model struct {
 
 // x[0] is the mean, x[1] is the log stddev of the distribution
 func (m *Model) Observe(x []float64) float64 {
-	return Normal.Logps(x[0], math.Exp(x[1]), m.Data...)
+    // Our prior is a unit normal ...
+    ll := Normal.Logps(0, 1, x...)
+    // ... but the posterior is based on data observations.
+	ll += Normal.Logps(x[0], math.Exp(x[1]), m.Data...)
+    return ll
 }
 ```
 
--- examples/hello/model/model.go
+++ examples/hello/model/model.go
@@ -12,7 +12,11 @@ type Model struct {
 	Data []float64
 }
 
-// x[0] is the mean, x[1] is the logvariance of the distribution
+// x[0] is the mean, x[1] is the log stddev of the distribution
 func (m *Model) Observe(x []float64) float64 {
-	return Normal.Logps(x[0], math.Exp(x[1]), m.Data...)
+    // Our prior is a unit normal ...
+    ll := Normal.Logps(0, 1, x...)
+    // ... but the posterior is based on data observations.
+	ll += Normal.Logps(x[0], math.Exp(x[1]), m.Data...)
+    return ll
 }
