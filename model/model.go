2345a50bdffcdae0fd310945e4fddd84724f839e
David Tolpin <david.tolpin@gmail.com>
2019-01-27 19:18:51 +0200

    singleton

--- model/model.go
+++ model/model.go
@@ -10,16 +10,24 @@ type Model interface {
 	Observe(parameters []float64) float64
 }
 
-// Desctructuring the parameters
+// Singleton T of type t is used to define utility functions useful for
+// implementing models.
+type t struct {}
+func (m t) Observe(x []float64) float64 {
+	return 0
+}
+var T t
+
+// Destructuring the parameters
 
 // Shiftn shifts n elements from the slice pointed to by px into
 // the slice pointed to by py.
-func (m Model) Shiftn(px *[]float64, n int, py *[]float64) {
+func (m t) Shiftn(px *[]float64, n int, py *[]float64) {
 	*py, *px = (*px)[:n], (*px)[n:]
 }
 
 // Shiftn shifts a single value from the slice pointed to by px into
 // the float64 variable pointed to by py.
-func (m Model) Shift(px *[]float64, py *float64) {
+func (m t) Shift(px *[]float64, py *float64) {
 	*py, *px = (*px)[0], (*px)[1:]
 }
