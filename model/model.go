c7840551eaa68c64fa241fa44f7b54d3ab59eec7
David Tolpin <david.tolpin@gmail.com>
2019-01-27 19:40:01 +0200

    removed shift and shiftn

--- model/model.go
+++ model/model.go
@@ -9,25 +9,3 @@ package model
 type Model interface {
 	Observe(parameters []float64) float64
 }
-
-// Singleton T of type t is used to define utility functions useful for
-// implementing models.
-type t struct {}
-func (m t) Observe(x []float64) float64 {
-	return 0
-}
-var T t
-
-// Destructuring the parameters
-
-// Shiftn shifts n elements from the slice pointed to by px into
-// the slice pointed to by py.
-func (m t) Shiftn(px *[]float64, n int, py *[]float64) {
-	*py, *px = (*px)[:n], (*px)[n:]
-}
-
-// Shiftn shifts a single value from the slice pointed to by px into
-// the float64 variable pointed to by py.
-func (m t) Shift(px *[]float64, py *float64) {
-	*py, *px = (*px)[0], (*px)[1:]
-}
