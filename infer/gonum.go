4ae1796db2cbffab8f41b1bf69ce3c803cab6b71
David Tolpin <david.tolpin@gmail.com>
2019-03-19 21:59:33 +0200

    FuncGrad, properly documented

--- infer/gonum.go
+++ infer/gonum.go
@@ -9,9 +9,9 @@ import (
 // The autodiff tape is not re-entrant, we need a mutex.
 var tapeMutex = sync.Mutex{}
 
-// FuncGrad returns the function to minimize and 
-// the gradient, suitable as fields for gonum
-// optimize.Problem.
+// FuncGrad returns the function to minimize and the gradient,
+// suitable as fields for gonum optimize.Problem, corresponding
+// to maximization of the model's log-likelihood.
 func FuncGrad(m model.Model) (
 	func(x []float64) float64,
 	func(grad []float64, x []float64) []float64,
