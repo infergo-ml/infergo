8fcd8627a93f0af37e6bbfc67ed9697cfe62831b
David Tolpin <david.tolpin@gmail.com>
2019-03-19 21:56:14 +0200

    FuncGrad

---
+++ infer/gonum.go
@@ -0,0 +1,41 @@
+package infer
+
+import (
+	"bitbucket.org/dtolpin/infergo/ad"
+	"bitbucket.org/dtolpin/infergo/model"
+	"sync"
+)
+
+// The autodiff tape is not re-entrant, we need a mutex.
+var tapeMutex = sync.Mutex{}
+
+// FuncGrad returns the function to minimize and 
+// the gradient, suitable as fields for gonum
+// optimize.Problem.
+func FuncGrad(m model.Model) (
+	func(x []float64) float64,
+	func(grad []float64, x []float64) []float64,
+) {
+	Func :=  func(x []float64) float64 {
+		defer tapeMutex.Unlock()
+		tapeMutex.Lock()
+		ll := m.Observe(x)
+		ad.Pop()
+		return -ll
+	}
+
+	Grad := func(grad []float64, x []float64) []float64 {
+		defer tapeMutex.Unlock()
+		tapeMutex.Lock()
+		_, grad_ := m.Observe(x), ad.Gradient()
+		if grad == nil {
+			grad = grad_
+		}
+		for i := range grad_ {
+			grad[i] = -grad_[i]
+		}
+		return grad
+	}
+
+	return Func, Grad
+}
