470cac4a7568eac2308180e08ad6c4609097ee78
David Tolpin <david.tolpin@gmail.com>
2019-03-29 17:21:14 +0300

    mutex in gonum is optional

--- infer/gonum.go
+++ infer/gonum.go
@@ -6,13 +6,37 @@ import (
 	"sync"
 )
 
-// The autodiff tape is not re-entrant, we need a mutex.
-var tapeMutex = sync.Mutex{}
-
 // FuncGrad returns the function to minimize and the gradient,
 // suitable as fields for gonum optimize.Problem, corresponding
 // to maximization of the model's log-likelihood.
 func FuncGrad(m model.Model) (
+	func(x []float64) float64,
+	func(grad []float64, x []float64) []float64,
+) {
+	Func := func(x []float64) float64 {
+		ll := m.Observe(x)
+		ad.Pop()
+		return -ll
+	}
+
+	Grad := func(grad []float64, x []float64) []float64 {
+		_, grad_ := m.Observe(x), ad.Gradient()
+		if grad == nil {
+			grad = grad_
+		}
+		for i := range grad_ {
+			grad[i] = -grad_[i]
+		}
+		return grad
+	}
+
+	return Func, Grad
+}
+
+// FuncGradM is a mutexed version of FuncGrad. Should be used
+// when the inference algorithm is parallelized. The model must
+// be immutable.
+func FuncGradM(m model.Model) (
 	func(x []float64) float64,
 	func(grad []float64, x []float64) []float64,
 ) {
@@ -39,3 +63,6 @@ func FuncGrad(m model.Model) (
 
 	return Func, Grad
 }
+
+// Tape mutex for FuncGradM
+var tapeMutex = sync.Mutex{}
