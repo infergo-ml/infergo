f45354032f3b0cb1318dc876ed3df5cc1bd07cbd
David Tolpin <david.tolpin@gmail.com>
2020-07-27 12:14:32 +0300

    reformatted and rearranged

--- infer/grad.go
+++ infer/grad.go
@@ -119,25 +119,24 @@ func (opt *Adam) setDefaults() {
 // plateau is reached.
 func Optimize(
 	opt Grad,
-	m model.Model, theta []float64, 
+	m model.Model, x []float64,
 	niter, nplateau int,
 	eps float64,
 ) (
-	ll0, ll float64,
 	iter int,
+	ll0, ll float64,
 ) {
-	ll0, _ = opt.Step(m, theta)
-	ll, llprev := ll0, ll0
-	iter = 0
-	plateau := 0
+	ll0, _ = opt.Step(m, x)
+	iter, ll = 0, ll0
+	plateau, llprev := 0, ll0
 	// Evolve x_, keep x_ with the highest log-likelihood in x
-	theta_ := make([]float64, len(theta))
-	copy(theta_, theta)
+	x_ := make([]float64, len(x))
+	copy(x_, x)
 	for ; iter != niter; iter++ {
-		ll_, _ := opt.Step(m, theta_)
-		// Store theta_ in theta if log-likelihood increased
+		ll_, _ := opt.Step(m, x_)
+		// Store x_ in x if log-likelihood increased
 		if ll_ > ll {
-			copy(theta, theta_)
+			copy(x, x_)
 			ll = ll_
 		}
 		// Stop early if the optimization is stuck
@@ -152,5 +151,5 @@ func Optimize(
 		llprev = ll_
 	}
 
-	return ll0, ll, iter
+	return iter, ll0, ll
 }
